2629cd342cacf6002d95c898962b8addb164e885
William24A <willi241194@gmail.com>
2023-11-03 19:40:40 -0300

    commir repusuario

--- Repositorio/RepoUsuario.cs
+++ Repositorio/RepoUsuario.cs
@@ -1,33 +1,94 @@
-
+using System.Data.SQLite;
 using UtilizarUsuario;
 
 namespace RepoUsuarioU
 {
     public class RepoUsuarioC : IDUsuarioRepository
     {
+        private string cadenaConexion = "Data Source=DB/kanban.db;Cache=Shared";
         public void Create(Usuario usuario)
         {
-            throw new NotImplementedException();
+            var query = $"INSERT INTO Usuario (nombre_de_usuario) VALUES (@name)";
+            using (SQLiteConnection connection = new SQLiteConnection(cadenaConexion))
+            {
+
+                connection.Open();
+                var command = new SQLiteCommand(query, connection);
+
+                command.Parameters.Add(new SQLiteParameter("@name", usuario.NombreUsuario));
+
+                command.ExecuteNonQuery();
+
+                connection.Close(); 
+            }
         }
 
         public List<Usuario> GetAll()
         {
-            throw new NotImplementedException();
+            var queryString = @"SELECT * FROM Usuario;";
+            List<Usuario> usuarios = new List<Usuario>();
+            using (SQLiteConnection connection = new SQLiteConnection(cadenaConexion))
+            {
+                SQLiteCommand command = new SQLiteCommand(queryString, connection);
+                connection.Open();
+            
+                using(SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var usuario = new Usuario();
+                        usuario.Id = Convert.ToInt32(reader["id"]);
+                        usuario.NombreUsuario = reader["nombre_de_usuario"].ToString();
+                        usuarios.Add(usuario);
+                    }
+                }
+                connection.Close();
+            }
+            return usuarios;
         }
 
         public Usuario GetById(int id)
         {
-            throw new NotImplementedException();
+            SQLiteConnection connection = new SQLiteConnection(cadenaConexion);
+            var usuario = new Usuario();
+            SQLiteCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Usuario WHERE id = @idUsuario";
+            command.Parameters.Add(new SQLiteParameter("@idUsuario", id));
+            connection.Open();
+            using(SQLiteDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    usuario.Id = Convert.ToInt32(reader["id"]);
+                    usuario.NombreUsuario = reader["nombre_de_usuario"].ToString();
+                }
+            }
+            connection.Close();
+
+            return usuario;
         }
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            SQLiteConnection connection = new SQLiteConnection(cadenaConexion);
+            SQLiteCommand command = connection.CreateCommand();
+            command.CommandText = $"DELETE FROM Usuario WHERE id = @idUsuario;";
+            command.Parameters.Add(new SQLiteParameter("@idUsuario", id));
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
         }
 
         public void Update(Usuario usuario)
         {
-            throw new NotImplementedException();
+            SQLiteConnection connection = new SQLiteConnection(cadenaConexion);
+            SQLiteCommand command = connection.CreateCommand();
+            command.CommandText = $"UPDATE Usuario SET name = @nombre_de_usuario WHERE id = @idUsuario;";
+            command.Parameters.Add(new SQLiteParameter("@idUsuario", usuario.Id));
+            command.Parameters.Add(new SQLiteParameter("@nombre_de_usuario",usuario.NombreUsuario));
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
         }
     }
 }
